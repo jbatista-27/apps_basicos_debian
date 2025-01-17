7b4eff0235a57c677423fc5dca01a24b74f65847
Jefferson Batista <113936349+jbatista-27@users.noreply.github.com>
2025-01-17 17:22:13 -0300

    Create basic_install_app_debian.sh

---
+++ basic_install_app_debian.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo ""
+echo "Inciando Script..."
+apt update
+
+apt install vim -y
+apt install cockpit -y
+apt install bash-completion -y
+apt install aptitude -y
+apt install sshpass -y
+
+
+echo "Deseja instalar o Firewall UFW? (s/n)"
+read resposta
+
+if [ $resposta = 's' ]; then
+
+apt install ufw -y
+
+fi
+
+echo ""
+echo ""
+echo "Para informacoes sobre os apps instalados abrar o arquivo Sobre.txt"
