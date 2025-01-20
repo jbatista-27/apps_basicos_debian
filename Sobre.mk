bd24e0e9209e98a400bf3b10c5cf9e713bf23df6
Jefferson Batista <113936349+jbatista-27@users.noreply.github.com>
2025-01-20 09:06:20 -0300

    Create Sobre.mk

---
+++ Sobre.mk
@@ -0,0 +1,13 @@
+# Atualiza os Repositórios do Linux
+
+>Utiliza o comando **apt update** para atualizar a lista de pacotes linux
+
+# Lista de Pacotes para Instalação 
+
+**1º vim** - Editor de texto
+**2º cockpit** - Gerenciador de administração web
+**3º bash-completion** - Auto-completa comandos no debian
+**4º aptitude** - Gerenciador de pacotes complementar ao apt-get
+**5º sshpass** - Server para passar a senha como parâmetro
+**6º timeshift** - Cria snapshot do S.O
+**7º ufw** - Firewall para linux
